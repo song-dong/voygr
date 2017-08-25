2cb7bbc24e09edaaed3b9f85f8cdd7e018485fee
金成東 <songdong0204@gmail.com>
2017-08-25 18:38:20 +0900

    Front side

---
+++ form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>beasone</title>
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+</head>
+
+<body>
+    <header>
+        
+    </header>
+    
+    <section>
+        
+    </section>
+    
+    <footer>
+        
+    </footer>
+</body>
+
+
+</html>
